98631b6443b8d168c33f769d5a419e15fcae4ed7
David Duarte (Williams) <davidduart04@gmail.com>
2024-08-16 14:43:20 -0300

    copiando arquivos do spring

---
+++ src/test/java/com/david/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
